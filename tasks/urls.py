4060f2a2fb03236d90bb09c5aa960da5003a25f5
Samuel Santos <samuxlh@gmail.com>
2023-11-15 23:30:59 -0300

    updating name convention

--- tasks/urls.py
+++ tasks/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("college", views.index)
+    path("college", views.college)
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -4,5 +4,5 @@ from django.http import HttpResponse
 # Create your views here.
 
 
-def index(request):
-    return HttpResponse("Hello World")
+def college(request):
+    return HttpResponse("'College' view")
